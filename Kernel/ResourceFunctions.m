e663de7826cf7500074b5be63fed6760b0f78bed
Nikolay Murzin <nikm@wolfram.com>
2023-03-10 13:20:29 +0300

    add hypergraph rule WFRs

--- Kernel/ResourceFunctions.m
+++ Kernel/ResourceFunctions.m
@@ -2,10 +2,16 @@ Package["WolframInstitute`Hypergraph`"]
 
 PackageExport["RandomHypergraph"]
 PackageExport["EnumerateHypergraphs"]
+PackageExport["RandomHypergraphRule"]
+PackageExport["EnumerateHypergraphRules"]
 
 
 
 RandomHypergraph[args___, opts : OptionsPattern[]] := Hypergraph[ResourceFunction["RandomHypergraph"][args], opts]
 
 EnumerateHypergraphs[args___, opts : OptionsPattern[]] := Hypergraph[#, opts] & /@ ResourceFunction["EnumerateHypergraphs"][args]
+
+RandomHypergraphRule[args___, opts : OptionsPattern[]] := HypergraphRule[##, opts] & @@ ResourceFunction["RandomWolframModel"][args]
+
+EnumerateHypergraphRules[args___, opts : OptionsPattern[]] := HypergraphRule[##, opts] & @@@ ResourceFunction["EnumerateWolframModelRules"][args]
 
