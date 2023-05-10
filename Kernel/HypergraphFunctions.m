01b3432075eebfd14e35f787400d3dbf2ebe7e03
Nikolay Murzin <nikm@wolfram.com>
2023-05-10 16:18:30 +0300

    fix annotations for VertexReplace

--- Kernel/HypergraphFunctions.m
+++ Kernel/HypergraphFunctions.m
@@ -4,12 +4,17 @@ PackageExport["HypergraphIncidence"]
 PackageExport["CanonicalHypergraph"]
 PackageExport["IsomorphicHypergraphQ"]
 PackageExport["EdgeSymmetry"]
+PackageExport["EdgeListTagged"]
 
 
 
 EdgeList[hg_Hypergraph ? HypergraphQ] ^:= hg["EdgeList"]
 
 EdgeList[hg_Hypergraph ? HypergraphQ, patt_] ^:= Cases[hg["EdgeList"], patt]
+
+EdgeListTagged[hg_Hypergraph ? HypergraphQ] := hg["EdgeListTagged"]
+
+EdgeListTagged[hg_Hypergraph ? HypergraphQ, patt_] := Cases[hg["EdgeListTagged"], patt]
 
 EdgeCount[hg_Hypergraph ? HypergraphQ] ^:= Length @ EdgeList[hg]
 
@@ -37,7 +42,7 @@ mapEdgeOptions[f_, opt_] := Replace[Flatten[{opt}], {((edge_List -> tag_) -> v_)
 mapVertexOptions[f_, opt_] := Replace[Flatten[{opt}], (vertex_ -> v_) :> f[vertex] -> v, {1}]
 
 
-CanonicalHypergraph[hg_ ? HypergraphQ] := Block[{vs = hg["VertexList"], edges = hg["EdgeList"], newVertices, emptyEdges, newEdges, iso, perm},
+CanonicalHypergraph[hg_ ? HypergraphQ] := Block[{vs = VertexList[hg], edges = EdgeList[hg], newVertices, emptyEdges, newEdges, iso, perm},
     emptyEdges = Cases[edges, {}];
     edges = DeleteCases[edges, {}];
     {perm, iso} = ResourceFunction["FindCanonicalHypergraphIsomorphism"][edges, "IncludePermutation" -> True];
@@ -46,7 +51,7 @@ CanonicalHypergraph[hg_ ? HypergraphQ] := Block[{vs = hg["VertexList"], edges = 
     Hypergraph[
         newVertices, Join[emptyEdges, newEdges],
         With[{keys = Keys[hg["Options"]]},
-            Association @ hg["Options"] //
+            KeySort @ Association @ hg["Options"] //
                 MapAt[
                     Sort @ mapEdgeOptions[Replace[#, iso, {1}] &, #] &,
                     {Key[#]} & /@ Intersection[{EdgeStyle, EdgeLabels, EdgeLabelStyle, "EdgeSymmetry"}, keys]
@@ -68,8 +73,19 @@ IsomorphicHypergraphQ[hg1_ ? HypergraphQ, hg2_ ? HypergraphQ] :=
 
 
 VertexReplace[hg_Hypergraph, rules_] ^:= Hypergraph[
-    Replace[VertexList[hg], rules, {1}],
-    Hyperedges @@ Replace[EdgeList[hg], rules, {2}],
-    hg["Options"]
+    DeleteDuplicates @ Replace[VertexList[hg], rules, {1}],
+    Hyperedges @@ (Replace[{(edge_ -> tag_) :> Replace[edge, rules, {1}] -> tag, edge_ :> Replace[edge, rules, {1}]}] /@ EdgeListTagged[hg]),
+    Replace[
+        hg["Options"], {
+            (opt : (VertexStyle | VertexLabels | VertexLabelStyle | VertexCoordinates) -> annotation_List) :>
+                opt -> Replace[annotation, {(h : Rule | RuleDelayed)[v_, a_] :> h[Replace[v, rules], a]}, {1}],
+            (opt : (EdgeStyle | EdgeLabels | EdgeLabelStyle | "EdgeSymmetry") -> annotation_List) :>
+                opt -> Replace[annotation, {(h : Rule | RuleDelayed)[e_, a_] :> h[Replace[e, rules, {1}], a]}, {1}]
+        },
+        {1}
+    ]
 ]
+
+VertexReplace[rules_][hg_Hypergraph] ^:= VertexReplace[hg, rules]
+
 
