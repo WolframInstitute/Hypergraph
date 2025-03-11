2d7608d6ce37c7749b75fd558d8c795449631738
Nikolay Murzin <nikm@wolfram.com>
2025-03-11 19:50:33 +0300

    add new random hypergraph functions

--- Kernel/HypergraphGeneration.m
+++ Kernel/HypergraphGeneration.m
@@ -4,6 +4,8 @@ PackageExport["EnumerateHypergraphs"]
 PackageExport["EnumerateOrderedHypergraphs"]
 
 PackageExport["RandomHypergraph"]
+PackageExport["RandomAllHypergraph"]
+PackageExport["RandomConnectedHypergraph"]
 
 
 
@@ -48,7 +50,7 @@ EnumerateHypergraphs[s : _Integer ? Positive | Automatic | All | {_Integer ? Pos
 
 
 
-Options[RandomHypergraph] := Join[{"Simple" -> False, "Connected" -> False}, Options[Hypergraph]]
+Options[RandomHypergraph] := Join[{"Simple" -> True, "Connected" -> All}, Options[Hypergraph]]
 
 RandomHypergraph[s : _Integer ? Positive | Automatic : Automatic, sig_, opts : OptionsPattern[]] := Block[{
     hg,
@@ -59,8 +61,41 @@ RandomHypergraph[s : _Integer ? Positive | Automatic : Automatic, sig_, opts : O
         Switch[simple, All, True, True, SimpleHypergraphQ[hg], False, ! SimpleHypergraphQ[hg]] &&
             Switch[connected, All, True, True, ConnectedHypergraphQ[hg], False, ! ConnectedHypergraphQ[hg]],
 
-        hg = Hypergraph[ResourceFunction["RandomHypergraph"][{s, sig}], opts]
+        hg = Hypergraph[ResourceFunction["RandomHypergraph"][{s, sig}]]
     ];
-    hg
+    Hypergraph[hg, FilterRules[{opts}, Options[Hypergraph]]]
+]
+
+
+
+RandomAllHypergraph[{v : _Integer ? Positive | Automatic, sig : {{_Integer ? Positive, _Integer ? Positive} ...}}] := With[{
+	n = Replace[v, Automatic :> RandomInteger[{1, Total[Times @@@ sig]}]]
+},
+	Hypergraph[Range[n], Replace[sig, {m_, arity_} :> Splice @ RandomInteger[{1, n}, {m, arity}], 1]]
+]
+
+HyperConnectedComponents[edges_] := WeaklyConnectedComponents[UndirectedEdge @@@ Catenate[Partition[#, 2, 1] & /@ edges]]
+
+RandomConnectEdges[edges_] := Block[{newEdges = edges, components},
+	While[True,
+		components = HyperConnectedComponents[newEdges];
+		If[Length[components] == 1, Break[]];
+		newEdges = Replace[newEdges, RandomChoice[Catenate[#1]] -> RandomChoice[Catenate[#2]], {2}] & @@ TakeDrop[components, 1]
+	];
+	newEdges
+]
+
+RandomConnectedHypergraph[{v : _Integer ? Positive | Automatic, sig : {{_Integer ? Positive, _Integer ? Positive} ...}}] := With[{
+	n = Replace[v, Automatic :> RandomInteger[{1, Total[Times @@@ sig] - Total[sig[[All, 1]]] + 1}]]
+},
+	(*Hypergraph[RandomConnectEdges @ Replace[sig, {m_, arity_} :> Splice @ RandomInteger[{1, n}, {m, arity}], 1]]*)
+	Fold[
+		Block[{xs = RandomChoice[Union @@ #1, RandomInteger[{1, #2}]], ys, k},
+			k = #2 - Length[xs];
+			Append[#1, If[k == 0, xs, ys = Complement[Range[0, n - 1], xs]; RandomSample @ Join[xs, ys[[1 + ResourceFunction["TupleFromIndex"][RandomInteger[{1, Length[ys] ^ k}], k]]]]]]
+		] &,
+		{ResourceFunction["TupleFromIndex"][RandomInteger[{1, n ^ First[#]}], First[#]]},
+		Rest[#]
+	] & @ RandomSample[Replace[sig, {m_, arity_} :> Splice @ Table[arity, m], 1]]
 ]
 
